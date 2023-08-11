8b92af5af2647d5d8eb74f30fa9ae77e15cee468
Mehrshad Sahebsara <msahebsara@live.ca>
2023-08-11 00:24:35 -0600

    chore: update profile

--- data/profile.js
+++ data/profile.js
@@ -1,7 +1,7 @@
 const profile = {
   name: "Mehr Sahebsara",
   headline:
-    "Currently a Software Developer @ Intuit. Computing Science specialization graduate from the University of Alberta. I like to build and optimize full-stack applications, with a recent interest in front-end frameworks like Next.js and Tailwind that I used to build this personal website. In my free time, I enjoy biking, playing chess, and discovering new music.",
+    "Currently a Software Developer @ Intuit. Computing Science specialization graduate from the University of Alberta. I like to build and optimize full-stack applications, with a recent interest in LLMs and serverless computing. I built this website with Next.js and Tailwind and deployed it with Vercel, to learn more about these cool new technologies. In my free time, I enjoy cycling, playing chess, and discovering new music.",
   githubLink: "https://github.com/msahebsara",
   linkedinLink: "https://www.linkedin.com/in/msahebsara/",
 };
